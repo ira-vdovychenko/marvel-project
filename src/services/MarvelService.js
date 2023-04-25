08a61f88643e88289b5ae2007317ec53e7a1412a
Ira <vdovychenko.ire@gmail.com>
2023-04-25 15:27:14 +0300

    make it better

---
+++ src/services/MarvelService.js
@@ -0,0 +1,37 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=1812524e11367b4b67ec0572790157a7';
+    _baseOffet = 210;
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    }
+
+    getAllCharacters = async (offset = this._baseOffet) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url, 
+            comics: char.comics.items
+        }
+    }
+}
+
+export default MarvelService;
